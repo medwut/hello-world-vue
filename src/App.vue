0c0553b219212cd823a598bf32d03c4930e001b5
medwut <ahmed.h.maghrabi@gmail.com>
2022-08-31 17:02:24 +1000

    Hellow World Vue week4task
    this is my hellow world vue application for week 4 task
    pass task 1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> Hello World My name Is ahmed Maghrabi </h1>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
